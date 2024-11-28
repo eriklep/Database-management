adf1d4e182902310409d0ff7f00b00e6660792bd
eriklep <erik.leppanen1@gmail.com>
2024-11-28 19:14:17 +0200

    raw endpoints

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+
+app.listen(3001, () => {
+    console.log('Server is running in port 3001')
+});
+
+app.get('/genre', async (req,res) => {
+    res.send('genre')
+}); 
+
+app.get('/movie', async (req,res) => {
+    res.send('movie')
+});
+app.get('/user_', async (req,res) => {
+    res.send('user_')
+});
+app.get('/review', async (req,res) => {
+    res.send('review')
+});
+app.get('/favourite', async (req,res) => {
+    res.send('favourite')
+});
